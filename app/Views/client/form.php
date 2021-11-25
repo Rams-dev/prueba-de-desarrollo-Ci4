b21d1a683ead171a0cb4833aaf4e65fd19b31401
rams-dev <ramiroaltamirano.lpz@gmail.com>
2021-11-24 21:52:45 -0600

    required on form to fast validation

--- app/Views/client/form.php
+++ app/Views/client/form.php
@@ -1,18 +1,18 @@
 
 <div class="form-group mb-3">
     <label for="name">Nombre</label>
-    <input type="text" class="form-control" name="name" <?php if(isset($client["name"])){ ?> value="<?= $client["name"]?>" <?php } ?>>
+    <input required type="text" class="form-control" name="name" <?php if(isset($client["name"])){ ?> value="<?= $client["name"]?>" <?php } ?>>
 </div>
 
 <div class="form-group mb-3">
     <label for="lastname">Apellido paterno</label>
-    <input type="text" class="form-control" name="lastname" <?php if(isset($client["lastname"])){ ?> value="<?= $client["lastname"]?>" <?php } ?>>
+    <input required type="text" class="form-control" name="lastname" <?php if(isset($client["lastname"])){ ?> value="<?= $client["lastname"]?>" <?php } ?>>
 </div>
 <div class="form-group mb-3">
     <label for="lastname">Apellido materno</label>
-    <input type="text" class="form-control" name="mothers_lastname" <?php if(isset($client["mothers_lastname"])){ ?> value="<?= $client["mothers_lastname"]?>" <?php } ?>>
+    <input required type="text" class="form-control" name="mothers_lastname" <?php if(isset($client["mothers_lastname"])){ ?> value="<?= $client["mothers_lastname"]?>" <?php } ?>>
 </div>
 <div class="form-group mb-3">
     <label for="birthday">fecha de nacimiento</label>
-    <input type="date" class="form-control" name="birthday" <?php if(isset($client["birthday"])){ ?> value="<?= $client["birthday"]?>" <?php } ?>>
+    <input required type="date" class="form-control" name="birthday" <?php if(isset($client["birthday"])){ ?> value="<?= $client["birthday"]?>" <?php } ?>>
 </div>
